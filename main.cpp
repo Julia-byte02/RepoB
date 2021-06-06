b1e1c6c56b4d7741dfeea820f6c2d8a8f6e3468c
Julia <grazhuljulia@gmail.com>
2021-06-06 13:31:18 +0300

    Изменение

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main {
     int a,b;
-    printf("Введите два целых числа:");
+    printf("Введите два разных целых числа:");
     scanf("%d%d", &a, &b);
 
     printf("Сумма=%d", a+b);
